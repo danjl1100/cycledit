4cc08ceb248199c14ec2a0f22177fcd33fcf6df3
Daniel Lambert <danjl1100@gmail.com>
2026-03-10 19:04:10 -0500

    clarify placeholder code is a placeholder

--- src/bin/cycledit.rs
+++ src/bin/cycledit.rs
@@ -1,3 +1,4 @@
 fn main() {
+    // sample code, to be removed/replaced with actual logic
     println!("hello world! {}", cycledit::add(2, 3));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// sample code, to be removed/replaced with actual logic
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
