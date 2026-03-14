38a38102f1f6ec3bc544b4eda2af7d596a913284
Daniel Lambert <danjl1100@gmail.com>
2026-03-14 16:48:00 -0500

    change dump-fixture tests main output to snapshots

--- tests/integration/dump_fixture_tests.rs
+++ tests/integration/dump_fixture_tests.rs
@@ -13,10 +13,9 @@ fn round_trip_single_add() -> eyre::Result<()> {
     let list_output = TestHarness::new()?
         .init_git(&dumped)?
         .run_cli("2026-01-01T00:00:00+00:00[UTC]", &["list"])?;
-    assert_eq!(
+    insta::assert_snapshot!(
         list_output.stdout,
-        "2024-01-15 foo.txt
-"
+        @"2024-01-15 foo.txt"
     );
     Ok(())
 }
@@ -38,11 +37,12 @@ fn round_trip_add_and_remove() -> eyre::Result<()> {
     let harness2 = TestHarness::new()?.init_git(&dumped)?;
     assert_eq!(harness2.commit_count()?, 2);
     let list_output = harness2.run_cli("2026-01-01T00:00:00+00:00[UTC]", &["list"])?;
-    assert_eq!(
+    insta::assert_snapshot!(
         list_output.stdout,
-        "2024-03-20 bar.txt
-2024-01-15 foo.txt
-"
+        @r"
+    2024-03-20 bar.txt
+    2024-01-15 foo.txt
+    "
     );
     Ok(())
 }
@@ -66,14 +66,15 @@ fn round_trip_multiple_files() -> eyre::Result<()> {
     let harness2 = TestHarness::new()?.init_git(&dumped)?;
     assert_eq!(harness2.commit_count()?, 2);
     let list_output = harness2.run_cli("2026-01-01T00:00:00+00:00[UTC]", &["list"])?;
-    assert_eq!(
+    insta::assert_snapshot!(
         list_output.stdout,
-        "2024-01-15 aaa.txt
-2024-03-20 aaa2.txt
-2024-03-20 bbb.txt
-2024-01-15 mmm.txt
-2024-01-15 zzz.txt
-"
+        @r"
+    2024-01-15 aaa.txt
+    2024-03-20 aaa2.txt
+    2024-03-20 bbb.txt
+    2024-01-15 mmm.txt
+    2024-01-15 zzz.txt
+    "
     );
     Ok(())
 }
